ed22aabb200825e2214281f5f99b80f7db806c97
shahnaz0101 <126901554+shahnaz0101@users.noreply.github.com>
2023-03-16 07:54:54 -0700

    ttttttttttttttttttttttt

---
+++ TASK1/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+
+            Employee firstemployee = new Employee()
+            {
+                Name = "fij",                
+            };
+            employees[0] = firstemployee;
+
+            Employee secondemployee = new Employee()
+            {
+                Name="iojgoi",
+            };
+            employees[1] = secondemployee;
+
+            Employee thridemployee=new Employee()
+            {
+                Name = "iejfoie"
+            };
+            employees[2] = thridemployee;
+
+        }
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("sound of " + Name);
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int ID { get; set; }
+        public string Department { get; set; }
+        public string Team { get; set; }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Name of employee is " + Name);
+        }
+
+    }
+
+
+
+
+    
+
+}
